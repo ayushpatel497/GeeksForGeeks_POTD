d9a66aa7b302568c62e00f3d347da9d7a24cc5d8
Ayush Patel <155339843+ayushpatel497@users.noreply.github.com>
2024-01-04 10:14:09 +0530

    Add files via upload

---
+++ Find_Element_Occuring_Once_When_All_Other_Are_Occuring_Thrice.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+    int singleElement(int arr[] ,int N) {
+        // code here
+        int single = 0;
+        int couple = 0;
+        for(int i=0; i<N; i++) {
+            single = (single ^ arr[i]) & ~couple;
+            couple = (couple ^ arr[i]) & ~single;
+        }
+        return single;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        
+        cin>>N;
+        int arr[N];
+        
+        for(int i=0 ; i<N ; i++)
+            cin>>arr[i];
+
+        Solution ob;
+        cout<<ob.singleElement(arr,N);
+        cout<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
