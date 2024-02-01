fb3037002a3238567028773f5c5eb7362c0483ab
Ayush Patel <155339843+ayushpatel497@users.noreply.github.com>
2024-02-01 13:16:33 +0530

    Add files via upload

---
+++ 32_Panagram_Checking.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution
+{
+  public:
+    //Function to check if a string is Pangram or not.
+    bool checkPangram (string s) {
+        // your code here
+        unordered_map<char,int>um;
+        for(auto i:s)
+        {
+            int temp=int(i);
+            if((temp<65 or temp>90) and (temp<97 or temp>122))
+                continue;
+            if(i>='A' and i<='Z')
+                i+=32;
+            um[i]++;
+        }
+        return um.size()==26;
+    }
+
+};
+
+//{ Driver Code Starts.
+
+// Driver Program to test above functions
+int main()
+{
+    int t;
+    cin>>t;
+    cin.ignore(INT_MAX, '\n');
+    while(t--){
+        string s;
+        getline(cin, s);
+        Solution obj;
+        if (obj.checkPangram(s) == true)
+            cout<<1<<endl;
+        else
+            cout<<0<<endl;
+    }
+    return(0);
+}
+
+// } Driver Code Ends
