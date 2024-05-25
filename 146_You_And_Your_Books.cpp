bd6395e06ff71712ffda3bfbcf55c6367707a851
Ayush Patel <155339843+ayushpatel497@users.noreply.github.com>
2024-05-25 09:04:56 +0530

    Add files via upload

---
+++ 146_You_And_Your_Books.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+#include <algorithm>
+#include <cstdio>
+#include <iostream>
+//#include<Windows.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    /*You are requried to complete this method */
+    long long max_Books(int arr[], int n, int k) {
+        // Your code here
+        long long maxsum=0,currentsum=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]<=k)
+                currentsum+=arr[i];
+            else 
+                currentsum=0;
+            maxsum=max(maxsum,currentsum);
+        }
+        return maxsum;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+        int ar[n];
+        for (int i = 0; i < n; i++) {
+            cin >> ar[i];
+        }
+        Solution ob;
+        cout << ob.max_Books(ar, n, k) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
