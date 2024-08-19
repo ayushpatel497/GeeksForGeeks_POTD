8bc1228eb1cbe18cf2c11ee8650606200f8bf5ba
Ayush Patel <155339843+ayushpatel497@users.noreply.github.com>
2024-08-19 09:06:39 +0530

    Add files via upload

---
+++ 232_Kth_Smallest.cpp
@@ -0,0 +1,74 @@
+//{ Driver Code Starts
+// Initial function template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    // arr : given array
+    // k : find kth smallest element and return using this function
+    int kthSmallest(vector<int> &arr, int k) {
+        // code here
+        int max_element = arr[0];
+        int n = arr.size();
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > max_element) {
+                max_element = arr[i];
+            }
+        }
+
+        int freq[max_element + 1] = {0};
+        for (int i = 0; i < n; i++) {
+            freq[arr[i]]++;
+        }
+
+        int count = 0;
+        for (int i = 0; i <= max_element; i++) {
+            if (freq[i] != 0) {
+                count += freq[i];
+                if (count >= k) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int test_case;
+    cin >> test_case;
+    cin.ignore();
+    while (test_case--) {
+
+        int k;
+        vector<int> arr, brr, crr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        getline(cin, input);
+        ss.clear();
+        ss.str(input);
+        while (ss >> number) {
+            crr.push_back(number);
+        }
+        k = crr[0];
+        int n = arr.size();
+        Solution ob;
+        cout << ob.kthSmallest(arr, k) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
