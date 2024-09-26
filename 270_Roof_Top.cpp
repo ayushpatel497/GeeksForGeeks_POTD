19b893c2a6f0827d30069d77825c831b13dbc422
Ayush Patel <155339843+ayushpatel497@users.noreply.github.com>
2024-09-26 12:18:39 +0530

    Add files via upload

---
+++ 270_Roof_Top.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    // Function to find maximum number of consecutive steps
+    // to gain an increase in altitude with each step.
+    int maxStep(vector<int>& arr) {
+        // Your code here
+        int c = 0;
+        int m = 0;
+        for (int i = 1; i < arr.size(); i++) {
+            if (arr[i] > arr[i - 1])
+                c++;
+            else {
+                m = max(c, m);
+                c = 0;
+            }
+        }
+        m = max(c, m);
+        return m;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input); // Read the entire line for the array elements
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution ob;
+        cout << ob.maxStep(arr) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
