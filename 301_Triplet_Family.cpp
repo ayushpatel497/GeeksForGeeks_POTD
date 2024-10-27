43e4b77b9ef82c891b2eb1a772ccc5758c316abf
Ayush Patel <155339843+ayushpatel497@users.noreply.github.com>
2024-10-27 20:57:25 +0530

    Add files via upload

---
+++ 301_Triplet_Family.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    bool findTriplet(vector<int>& arr) {
+        // Your code
+        int n = arr.size();
+        vector<int> result;
+        sort(arr.begin(), arr.end());
+        for (int i = n - 1; i >= 0; i--) {
+            int j = 0;
+            int k = i - 1;
+            while (j < k) {
+                if (arr[i] == arr[j] + arr[k]) {
+                    return true;
+                }
+                else if (arr[i] > arr[j] + arr[k])
+                    j += 1;
+                else
+                    k -= 1;
+            }
+        }
+        return false;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        bool res = obj.findTriplet(arr);
+        if (res)
+            cout << "true" << endl;
+        else
+            cout << "false" << endl;
+        // cout << res << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
